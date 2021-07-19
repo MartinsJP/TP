e07bf72ec08de67bcf128e85a858e00153c2b09d
MartinsJP <75331673+MartinsJP@users.noreply.github.com>
2021-07-19 18:07:50 -0300

    x2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
     {
         "particles": {
             "number": {
-                "value": 1000,
+                "value": 600,
                 "density": {
                     "enable": true,
                     "value_area": 800
